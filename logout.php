da95c46ddf937ead6ab807f15753d6088daf5e5e
Hamza Sehouli <sehouli.hamza@gmail.com>
2021-10-19 15:14:04 +0100

    step 3, add logout function

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start(); // Initialize the session
+ 
+
+$_SESSION = array();// Unset all of the session variables the were stored during login and signup 
+ 
+
+session_destroy(); // Destroy the session.
+ 
+
+header("location: /final-php/login.php"); // Redirect to login page
+exit;
+?>
