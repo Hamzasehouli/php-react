6fedaf5d1176b1c1d40f241743269d731a951b16
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-03 16:09:25 +0100

    Add user routes, model and controller

---
+++ server/controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\controllers;
+
+class UserController
+{
+    public static function getAllUsers()
+    {
+        echo 'get all users';
+    }
+    public static function createUser()
+    {
+        echo 'create user';
+    }
+    public static function getUser()
+    {
+        echo 'get user';
+    }
+    public static function updateUser()
+    {
+        echo 'update user';
+    }
+    public static function deleteUser()
+    {
+        echo 'delete user';
+    }
+}
--- server/index.php
+++ server/index.php
@@ -6,7 +6,9 @@ header("Access-Control-Allow-Headers: *");
 require_once __DIR__ . '/vendor/autoload.php';
 use app\config\Database;
 use app\controllers\HouseController;
+use app\controllers\UserController;
 use app\routes\HouseRoute;
+use app\routes\UserRoute;
 
 $con = new Database();
 // $router = new Router();
@@ -22,7 +24,17 @@ if (str_starts_with($_SERVER['REQUEST_URI'], '/api/v1/houses')) {
     $houseRouter->post('/api/v1/houses/delete-house', [HouseController::class, 'deleteHouse']);
     $houseRouter->run();
     return;
+}
 
+if (str_starts_with($_SERVER['REQUEST_URI'], '/api/v1/users')) {
+    $userRouter = new UserRoute();
+    $userRouter->get('/api/v1/users', [UserController::class, 'getAllUsers']);
+    $userRouter->post('/api/v1/users', [UserController::class, 'createUser']);
+    $userRouter->get('/api/v1/users/get-user', [UserController::class, 'getUser']);
+    $userRouter->post('/api/v1/users/update-user', [UserController::class, 'updateUser']);
+    $userRouter->post('/api/v1/users/delete-user', [UserController::class, 'deleteUser']);
+    $userRouter->run();
+    return;
 }
 
 echo '404, route not found with this url: ' . $_SERVER['REQUEST_URI'] . '';
