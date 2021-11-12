1921425384427acb321e3d015d5f8fd78e4e5b1e
hamzasehouli <sehoulihamza@yahoo.com>
2021-11-12 15:41:10 +0100

    Create house model

--- server/models/HouseModel.php
+++ server/models/HouseModel.php
@@ -2,5 +2,39 @@
 
 class HouseModel
 {
+    public $id = '';
+    public $title = '';
+    public $price = '';
+    public $added_at = '';
+    public $adresse = '';
+    public $images = '';
+    public $description = '';
+    public $user = '';
+    public $area = '';
+    public $type = '';
+    public $discret = '';
+    public $state = '';
+    public function find($con)
+    {
+        $stmt = $con->prepare('SELECT * FROM houses');
+        return $stmt;
+    }
+    public function create($con)
+    {
+        $stmt = $con->prepare('INSERT INTO houses (title, price, city, adresse, images,description,user,area,type,discret,state) VALUES(:title, :price, :city, :adresse,:images,:description,:user,:area,:type,:discret,:state)');
+        return $stmt;
+    }
+    public function findOne($con)
+    {
+        $stmt = $con->prepare('SELECT * FROM houses WHERE id=:id');
+        return $stmt;
+    }
+    public function findOneAndUpdate()
+    {
 
+    }
+    public function findOneAndDelete()
+    {
+
+    }
 }
